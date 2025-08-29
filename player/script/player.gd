9476f8647622e159b4decf54a506ae0175f8d8c8
Talong2024 <grupotalong@gmail.com>
2025-08-29 21:58:19 +0800

    movement and idle animation

--- player/script/player.gd
+++ player/script/player.gd
@@ -1,10 +1,16 @@
 class_name Player
 extends CharacterBody2D
 
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction = Vector2.ZERO
 var move_speed: float = 500.0
+var state : String = "Idle"
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var sprite : Sprite2D = $Sprite2D 
 
 func _physics_process(delta):
-	var direction = Vector2.ZERO
+
 	direction.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	direction.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 
@@ -13,3 +19,22 @@ func _physics_process(delta):
 	
 	velocity = direction * move_speed
 	move_and_slide()
+	
+	UpdateAnimation()
+
+
+func SetDirection() -> bool:
+	
+	return true
+	
+func UpdateAnimation() -> void:
+	if direction != Vector2.ZERO:
+		animation_player.play("Walking animation")
+		if direction.x < 0:
+			sprite.flip_h = true
+		elif direction.x > 0:
+			sprite.flip_h = false
+	else:
+		animation_player.play("Idle_side")
+	
+	
